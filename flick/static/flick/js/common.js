2c01d999978332392d0e687dafb786d42bcdf471
Geoffrey Ko <gkiwi96@gmail.com>
2018-04-18 14:10:31 -0700

    move common js functions to common.js; add create-user html page

--- flick/static/flick/js/common.js
+++ flick/static/flick/js/common.js
@@ -20,5 +20,51 @@ var common = {
             }
         }
         return false;
-    }
+    },
+
+    /**
+     * Adds a red, bolded border around element's textbox if the field is empty.
+     *
+     * @param element
+     */
+    redBorderEmptyField: function(element) {
+        if (common.fieldEmpty(element)) {
+            $(element).addClass("text-box-invalid");
+        }
+        else {
+            $(element).removeClass("text-box-invalid");
+        }
+    },
+
+    /**
+     * Applies a red border around inputTextElement if it is empty on key-up.
+     *
+     * @param inputTextElement
+     */
+    requireFieldNonEmpty: function(inputTextElement) {
+        $(inputTextElement).keyup(function(){ common.redBorderEmptyField($(inputTextElement)); });
+    },
+
+    /**
+     * Examines all fields from selector -
+     * if any of them are empty, it will disable buttonElement (aesthetically and functionally).
+     *
+     * TODO: currently bound to mousemove - buggy, but works
+     *
+     * @param buttonElement
+     * @param selector - e.g., "form#FORM-ID :input"
+     */
+    disableButtonUnlessAllFieldsFilled: function(buttonElement, selector) {
+        $(selector).not(":button, :hidden").each(function(index, input) {
+            $(input).mousemove(function() {
+                if (!($(this).val())) {
+                    $(buttonElement).prop('disabled', true);
+                    $(buttonElement).addClass('disabled');
+                    return;
+                }
+                $(buttonElement).removeClass('disabled');
+                $(buttonElement).prop('disabled', false);
+            })
+        });
+    },
 };
